3075b081a3c2011b40aeba1c1176974ac717ebaa
laxman-dhami <dhamilaxman555@gmail.com>
2023-12-19 17:30:15 +0530

    creatinelements

---
+++ index.js
@@ -0,0 +1,5 @@
+const subHeading = document.createElement("h3");
+subHeading.textContent = "Buy high quality organic fruits online";
+subHeading.style.fontStyle = "italic";
+const headerDiv = document.getElementById("header");
+headerDiv.appendChild(subHeading);
