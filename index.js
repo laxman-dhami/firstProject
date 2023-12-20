3ab2f90ae5240f8f1bd5ebe26459f1f4f87ce8d5
laxman-dhami <dhamilaxman555@gmail.com>
2023-12-20 13:10:04 +0530

    filter

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const subHeading = document.createElement("h3");
-subHeading.textContent = "Buy high quality organic fruits online";
-subHeading.style.fontStyle = "italic";
-const headerDiv = document.getElementById("header");
-headerDiv.appendChild(subHeading);
+const form=document.querySelector('form')
+const fruits=document.querySelector('.fruits')
+form.addEventListener('submit',function(event){
+  event.preventDefault();
+  const fruitToAdd=document.querySelector('#fruit-to-add')
+  const newLi=document.createElement('li')
+  newLi.innerHTML=fruitToAdd.value+'<button class="delete-btn">x'
+  fruits.append(newLi)
+})
+fruits.addEventListener('click',function(event){
+  if(event.target.classList.contains('delete-btn')){
+     const fruitToDelete=event.target.parentElement;
+     fruits.removeChild(fruitToDelete)
+    
+  }
+ });
+ const filter=document.getElementById('filter')
+ filter.addEventListener('keyup',function(e){
+ const textEntered=e.target.value.toLowerCase();
+ const fruitItems=document.getElementsByClassName('fruit')
+ for(let i=0;i<fruitItems.length;i++){
+  const currentFruitText=fruitItems[i].firstChild.textContent.toLowerCase();
+  if(currentFruitText.indexOf(textEntered)===-1){
+    fruitItems[i].style.display='none';
+  }else{
+    fruitItems[i].style.display='flex'
+  }
+}
+ })
