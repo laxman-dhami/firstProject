daac027364155728b66d7e03e9182332acc32fc7
laxman-dhami <dhamilaxman555@gmail.com>
2024-01-22 12:01:10 +0530

    getreq

--- savetocrudcrud/index.js
+++ savetocrudcrud/index.js
@@ -23,10 +23,23 @@ function saveToCloud(event) {
     document.body.innerHTML=document.body.innerHTML+"<h4>Something went wrong</h4>"
     console.log(err)
    })
-    function showOnScreen(obj){
-        const parentEle=document.getElementById('listofitems')
-        parentEle.innerHTML=parentEle.innerHTML+`<li>${obj.name}-${obj.email}-${obj.tel}`
-    }
+    
 }
-
+window.addEventListener("DOMContentLoaded",()=>{
+    axios.get('https://crudcrud.com/api/91a5b00b4bf84e91bf92df051baebd7a/appointmentData')
+    .then(res=>{
+        console.log(res.data)
+        for(let i=0;i<res.data.length;i++){
+         showOnScreen(res.data[i])
+        }
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+    
+})
+function showOnScreen(obj){
+    const parentEle=document.getElementById('listofitems')
+    parentEle.innerHTML=parentEle.innerHTML+`<li>${obj.name}-${obj.email}-${obj.tel}`
+}
 
