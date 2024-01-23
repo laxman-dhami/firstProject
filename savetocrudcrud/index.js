6c83ba419fe2eba51e99b848c830101aa34db774
laxman-dhami <dhamilaxman555@gmail.com>
2024-01-23 11:48:35 +0530

    deleteFunc

--- savetocrudcrud/index.js
+++ savetocrudcrud/index.js
@@ -7,14 +7,16 @@ function saveToCloud(event) {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const tel = document.getElementById('tel').value;
+    const add=document.getElementById('add').value;
 
     
     const obj = {
         name: name,
         email: email,
-        tel: tel
+        tel: tel,
+        add:add
     };
-   axios.post('https://crudcrud.com/api/91a5b00b4bf84e91bf92df051baebd7a/appointmentData',obj)
+   axios.post('https://crudcrud.com/api/57d2170fc9604478a46a6bee5ce54708/appointmentData',obj)
    .then(res=>{
     showOnScreen(res.data)
     
@@ -23,10 +25,10 @@ function saveToCloud(event) {
     document.body.innerHTML=document.body.innerHTML+"<h4>Something went wrong</h4>"
     console.log(err)
    })
-    
+     
 }
 window.addEventListener("DOMContentLoaded",()=>{
-    axios.get('https://crudcrud.com/api/91a5b00b4bf84e91bf92df051baebd7a/appointmentData')
+    axios.get('https://crudcrud.com/api/57d2170fc9604478a46a6bee5ce54708/appointmentData')
     .then(res=>{
         console.log(res.data)
         for(let i=0;i<res.data.length;i++){
@@ -40,6 +42,40 @@ window.addEventListener("DOMContentLoaded",()=>{
 })
 function showOnScreen(obj){
     const parentEle=document.getElementById('listofitems')
-    parentEle.innerHTML=parentEle.innerHTML+`<li>${obj.name}-${obj.email}-${obj.tel}`
+    const childHTML = `<li id=${obj._id}>${obj.name}-${obj.email}-${obj.tel}-${obj.add}
+    <button class="delete-btn" onclick="deleteItem('${obj._id}')">Delete</button>
+    <button class="edit-btn" onclick="editItem('${obj._id}', '${obj.name}', '${obj.email}', '${obj.tel}', '${obj.add}')">Edit</button>
+</li>`;
+
+parentEle.innerHTML = parentEle.innerHTML + childHTML;
 }
 
+
+    const parentEle = document.getElementById('listofitems');
+    const childHTML = `<li id=${obj._id}>${obj.name}-${obj.email}-${obj.tel}-${obj.add}
+        <button class="delete-btn" onclick="deleteItem('${obj._id}')">Delete</button>
+        <button class="edit-btn" onclick="editItem('${obj._id}', '${obj.name}', '${obj.email}', '${obj.tel}', '${obj.add}')">Edit</button>
+    </li>`; // Added closing backtick
+
+    parentEle.appendChild(listItem);
+
+
+
+function deleteItem(objId) {
+    axios.delete(`https://crudcrud.com/api/57d2170fc9604478a46a6bee5ce54708/appointmentData/${objId}`)
+        .then((res) => {
+            removeUserFromScreen(objId)
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+            
+        function removeUserFromScreen(objId){
+            const parentEle=document.getElementById('listofitems'); // Change 'listofusers' to 'listofitems'
+            const childNodeToBeDeleted=document.getElementById(objId);
+            if(childNodeToBeDeleted){
+                parentEle.removeChild(childNodeToBeDeleted); // Corrected typo here
+            }
+        }
+        
+    }
