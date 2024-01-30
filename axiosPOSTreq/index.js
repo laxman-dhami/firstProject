4788cc54b9bdcb688748317dbe0e4d7be899962f
laxman-dhami <dhamilaxman555@gmail.com>
2024-01-30 19:52:08 +0530

    deteteUser

--- axiosPOSTreq/index.js
+++ axiosPOSTreq/index.js
@@ -18,9 +18,9 @@ function savetoCrudcrud(event){
     console.log(err)
   })
 }
+
+
 window.addEventListener("DOMContentLoaded",()=>{
-
-
   axios.get('https://crudcrud.com/api/7bbb44ec07ae44fb9a641e9b17602d72/appointmentdata')
   .then((res)=>{
     for(let i=0;i<res.data.length;i++){
@@ -31,7 +31,31 @@ window.addEventListener("DOMContentLoaded",()=>{
     console.log(err)
   })
 })
-function showOnScreen(obj){
-  const parentEle=document.getElementById('listofusers')
-  parentEle.innerHTML=parentEle.innerHTML+`<li>${obj.Name}- ${obj.Email}-${obj.Phone} <button onclick="deleteUser('${obj._id}')">Delete</button> <button onclick=editUser="editUser('${obj._id}')">Edit</button></li>`
+
+
+
+
+
+function showOnScreen(obj) {
+  const parentEle = document.getElementById('listofusers');
+  parentEle.innerHTML = parentEle.innerHTML + `<li id="${obj._id}">${obj.Name}- ${obj.Email}-${obj.Phone} <button onclick="deleteUser('${obj._id}')">Delete</button> <button onclick="editUser('${obj._id}')">Edit</button></li>`;
+}
+
+
+function deleteUser(objId){
+axios.delete(`https://crudcrud.com/api/7bbb44ec07ae44fb9a641e9b17602d72/appointmentdata/${objId}`)
+.then((res)=>{
+  removeUser(objId)
+})
+.catch((err)=>{
+  console.log(err)
+})
+
+function removeUser(objId){
+  const parentNode=document.getElementById('listofusers')
+  const childNodetobedeleted=document.getElementById(objId)
+  if(childNodetobedeleted){
+    parentNode.removeChild(childNodetobedeleted)
+  }
+}
 }
